479388b04139b8c8c328e72e233767c3c522382b
kauamota <kauamotasantos@gmail.com>
2022-10-23 01:17:05 -0300

    teclado tatil

---
+++ som/Buzzer/Buzzer.ino
@@ -0,0 +1,66 @@
+//Definindo o pino das teclas e do buzzer.
+#define buzzer 10
+#define botao1 2
+#define botao2 3
+#define botao3 4
+#define botao4 5
+#define botao5 6
+#define botao6 7
+#define botao7 8
+#define botao8 9
+#define botao9 11
+#define botao10 12
+#define botao11 13
+#define botao12 A0
+
+ 
+void setup() {
+  //Declarando o buzzer como saída de sinal, e os botões como entradas em estado "Pull UP", o que significa que seu valor lógico é 1.
+  pinMode(buzzer, OUTPUT);
+  pinMode(botao1, INPUT_PULLUP);
+  pinMode(botao2, INPUT_PULLUP);
+  pinMode(botao3, INPUT_PULLUP);
+  pinMode(botao4, INPUT_PULLUP);
+  pinMode(botao5, INPUT_PULLUP);
+  pinMode(botao6, INPUT_PULLUP);
+  pinMode(botao7, INPUT_PULLUP);
+  pinMode(botao8, INPUT_PULLUP);
+  pinMode(botao9, INPUT_PULLUP);
+  pinMode(botao10, INPUT_PULLUP);
+  pinMode(botao11, INPUT_PULLUP);
+  pinMode(botao12, INPUT_PULLUP);
+}
+
+void loop() {
+  
+  int valor1 = analogRead(A0); //Criando uma variável com o valor do pino analógico A0.
+  
+ if (digitalRead(botao1) == LOW) { //Se botão1 estiver pressionado, envie a seguinte frequência/nota para a caixa de som:
+    tone(buzzer,262);
+}else if(digitalRead(botao2) == LOW){ //O mesmo se repete para todos os outros.
+    tone(buzzer, 277);
+}else if(digitalRead(botao3) == LOW){
+    tone(buzzer, 294);
+}else if(digitalRead(botao4) == LOW){
+    tone(buzzer, 311);
+}else if(digitalRead(botao5) == LOW){
+    tone(buzzer, 330);
+}else if(digitalRead(botao6) == LOW){
+    tone(buzzer, 349);
+}else if(digitalRead(botao7) == LOW){
+    tone(buzzer, 370);
+}else if(digitalRead(botao8) == LOW){
+    tone(buzzer, 392);
+}else if(digitalRead(botao9) == LOW){
+    tone(buzzer, 415);
+}else if(digitalRead(botao10) == LOW){
+    tone(buzzer, 440);
+}else if(digitalRead(botao11) == LOW){
+    tone(buzzer, 466);
+}else if(valor1 <= 100){
+    tone(buzzer, 494);
+}else{
+   noTone(buzzer);
+}
+  
+}
